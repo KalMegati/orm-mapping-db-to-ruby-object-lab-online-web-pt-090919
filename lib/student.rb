ba2be12aed4a90555a8bd1d802535e56cf0f61f8
Srikalyan Megati <kalmegati@gmail.com>
2019-11-13 18:54:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,10 @@ class Student
 
   def self.new_from_db(row)
     student = self.new
-    binding.pry
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
+    binding.pry
   end
 
   def self.all
