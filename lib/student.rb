0b012edcc91943426169d094fe1445fdbfaaa7f1
Srikalyan Megati <kalmegati@gmail.com>
2019-11-13 19:01:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,10 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
     SQL
+    
+    DB[:conn].execute(sql).each{ |row|
+      self.new_from_db(row)
+    }
   end
 
   def self.find_by_name(name)
